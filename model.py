d0b2c493a9160aa901e30f9ceae4f42a6f98bf35
Ashith Raghunath <dranzer.ashi@gmail.com>
2018-02-26 12:31:48 +0000

    higher epoch

--- model.py
+++ model.py
@@ -74,5 +74,5 @@ model.add(Dense(43))
 model.add(Dense(1))
 
 model.compile(optimizer="adam", loss="mse")
-model.fit_generator(train_generator, samples_per_epoch=len(training_samples)*2, validation_data=validation_generator, nb_val_samples=len(validation_samples)*2,nb_epoch=3)
+model.fit_generator(train_generator, samples_per_epoch=len(training_samples)*2, validation_data=validation_generator, nb_val_samples=len(validation_samples)*2,nb_epoch=9)
 model.save('model.h5')
